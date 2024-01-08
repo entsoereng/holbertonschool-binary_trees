fca2b08f9e433ba17b30fc7343da0291ce4dfa82
entsoereng <entsoereng@aol.com>
2024-01-08 17:38:49 +0200

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,12 +8,10 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree)
-		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
-
-	return (0);
+if (tree)
+return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+return (0);
 }
-
 /**
  * binary_tree_height - Measures the height of a binary tree.
  * @tree: A pointer to the root node of the tree to measure the height.
@@ -22,12 +20,12 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree)
-	{
-		size_t l = 0, r = 0;
+if (tree)
+{
+size_t l = 0, r = 0;
 l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-		return ((l > r) ? l : r);
-	}
-	return (0);
+r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+return ((l > r) ? l : r);
+}
+return (0);
 }
